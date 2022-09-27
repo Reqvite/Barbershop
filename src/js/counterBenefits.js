42152c5f2ad3bb7e0be9a561d5a2e4433435091a
Reqvite <reqvite2233@gmail.com>
2022-09-27 20:58:59 +0300

    counter.js

--- src/js/counterBenefits.js
+++ src/js/counterBenefits.js
@@ -17,7 +17,13 @@ elNumber = 0;
    }, 1);
 }
 
-const checkCoordinates =() => {
+console.log(navigator.userAgent);
+const checkCoordinates = () => {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
+        .test(navigator.userAgent)) {
+        counter();
+         window.removeEventListener('scroll', checkCoordinates);
+    }
     const documentRect = document.documentElement.getBoundingClientRect();
     const clientHeight = document.documentElement.clientHeight;
     if (documentRect.top<= -800) {
