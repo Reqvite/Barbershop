f8d54e85e4b88d216285bc4d49f193516ddd1aaa
Reqvite <reqvite2233@gmail.com>
2022-09-27 21:53:47 +0300

    add examples, masters

--- src/js/counterBenefits.js
+++ src/js/counterBenefits.js
@@ -3,6 +3,19 @@ const spanRef = document.querySelectorAll('.benefits-list__numbers')
 let intervalId = null;
 elNumber = 0;
 
+function checkDevice() {
+       if (/Android|webOS|iPhone|iPad|iPod|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
+        .test(navigator.userAgent)) {
+        spanRef.forEach(el => {
+            el.textContent = el.dataset.num;
+        });
+       } else {
+           window.addEventListener('scroll', checkCoordinates);  
+    }   
+}
+
+checkDevice()
+
  function counter() {
      let num = 0;
      if(elNumber <= 3)
@@ -17,18 +30,10 @@ elNumber = 0;
    }, 1);
 }
 
-console.log(navigator.userAgent);
-const checkCoordinates = () => {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
-        .test(navigator.userAgent)) {
-        counter();
-         window.removeEventListener('scroll', checkCoordinates);
-    }
+function checkCoordinates (){
     const documentRect = document.documentElement.getBoundingClientRect();
-    const clientHeight = document.documentElement.clientHeight;
     if (documentRect.top<= -800) {
         counter()
         window.removeEventListener('scroll', checkCoordinates);
     } 
 }
-window.addEventListener('scroll', checkCoordinates)
